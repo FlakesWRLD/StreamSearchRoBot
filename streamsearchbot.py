d6fac3f1b4f980ece340d776110105013a8e1f59
果汁 <71187213+FristyFlakes@users.noreply.github.com>
2020-12-21 06:57:58 +0100

    Update streamsearchbot.py

--- streamsearchbot.py
+++ streamsearchbot.py
@@ -35,8 +35,8 @@ async def search(event):
 async def search(event):
     await event.reply(message=f'<b>Join Our Update Channel :</b> [Flix Bots](https://t.me/FlixBots)', parse_mode="HTML",
                       buttons=[
-                      [Button.url("Support Channel", url="https://t.me/FlixBots")],
-                      [Button.url("Support Group", url="https://t.me/MirrorZone")],
+                      [Button.url("Support Channel", f"https://t.me/FlixBots")],
+                      [Button.url("Support Group", f"https://t.me/MirrorZone")],
                               ]
                      )
 @torrentbot.on(events.InlineQuery(pattern=r"torrent (.*)"))
